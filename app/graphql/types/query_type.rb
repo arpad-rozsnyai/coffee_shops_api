0f12d3531e8afc92cc7019254e6acfb6ce7a5a1c
arpi <arpad.rozsnyai@gmail.com>
2026-09-02 21:17:30 +0300

    Add name to search params

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,8 +2,11 @@ module Types
   class QueryType < Types::BaseObject
     MAX_COFFEE_SHOPS = 5
 
-    field :coffee_shops, [ Types::CoffeeShopType ], null: false,
-      description: "All persisted coffee shops (capped at #{MAX_COFFEE_SHOPS})"
+    field :coffee_shops, [ Types::CoffeeShopType ], null: false do
+      description "All persisted coffee shops (capped at #{MAX_COFFEE_SHOPS}), optionally filtered by name"
+      argument :name, String, required: false,
+        description: "Case-insensitive partial match against the coffee shop name"
+    end
 
     field :coffee_shop, Types::CoffeeShopType, null: true do
       description "A single persisted coffee shop by id"
@@ -16,8 +19,9 @@ module Types
       argument :y, Float, required: true
     end
 
-    def coffee_shops
-      CoffeeShop.limit(MAX_COFFEE_SHOPS)
+    def coffee_shops(name: nil)
+      scope = name.present? ? CoffeeShop.name_contains(name) : CoffeeShop
+      scope.limit(MAX_COFFEE_SHOPS)
     end
 
     def coffee_shop(id:)
